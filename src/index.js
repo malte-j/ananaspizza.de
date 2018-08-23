2570258cac0d5b7e487b57834a7ed52cf8f4a3c4
Malte <skyguardian42@gmail.com>
2018-08-23 15:16:12 +0200

    initial commit; v2

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// function calcVH () {
+// 	var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+// 	document.querySelector('body').setAttribute("style", "height:" + vH + "px;");
+// }
+// window.onload = calcVH();
+// window.addEventListener('onorientationchange', calcVH, true);
+// window.addEventListener('resize', calcVH, true);
